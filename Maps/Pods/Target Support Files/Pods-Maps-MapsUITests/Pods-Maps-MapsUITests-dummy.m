355846c787a0e2032545713dbc963a4fb13d840d
vishalthakur <>
2021-12-09 11:10:23 +0530

    Google maps dependancy added

---
+++ Maps/Pods/Target Support Files/Pods-Maps-MapsUITests/Pods-Maps-MapsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Maps_MapsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Maps_MapsUITests
+@end
---
+++ Maps/Pods/Target Support Files/Pods-MapsTests/Pods-MapsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapsTests : NSObject
+@end
+@implementation PodsDummy_Pods_MapsTests
+@end
